5216c5c21f9e63262f2d8cb97e324f6be59e3078
abuhanif2020331005 <abuhanif05@student.sust.edu>
2025-06-05 15:01:10 +0600

    complete api endpoint for rf_Cric

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import xgboost
 from routers import passbuy
+from routers import rf_Cric
 
 
 app = FastAPI(title="Player Market Value Prediction")
@@ -8,3 +9,4 @@ app = FastAPI(title="Player Market Value Prediction")
 # app.include_router(random_forest.router, prefix="/predict", tags=["Random Forest"])
 app.include_router(xgboost.router)
 app.include_router(passbuy.router)
+app.include_router(rf_Cric.router)
---
+++ fastapi/routers/rf_Cric.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+import joblib
+import pandas as pd
+
+router = APIRouter()
+
+# Load your trained model
+rfr_model = joblib.load('models/rf_Cric.pkl')
+feature_names = joblib.load('models/cric_feature_names.pkl')
+
+@router.post("/predict/rf_cric")
+async def predict_rf_cric(input_data: dict):
+    
+    input_df = pd.DataFrame([input_data])
+
+ 
+    for col in feature_names:
+        if col not in input_df.columns:
+            input_df[col] = 0
+
+    
+    input_df = input_df[feature_names]
+
+   
+    prediction = rfr_model.predict(input_df)[0]
+
+    
+    prediction = float(prediction)
+
+    # Return the prediction in a JSON response
+    return JSONResponse(content={"prediction": prediction})
